6b7bbc309ec9c362b1dabe3a581ef3ccb048eec6
Marcin Nowicki <pr0d1r2@gmail.com>
2015-02-18 13:16:17 +0100

    Extract common rspec shared examples.

--- lib/bookingsync_application/spec_helper.rb
+++ lib/bookingsync_application/spec_helper.rb
@@ -1,3 +1,11 @@
+shared_examples_for :synced_model do
+  describe ".included_modules" do
+    subject { described_class.included_modules }
+
+    it { is_expected.to include Synced::HasSyncedData }
+  end
+end
+
 require 'vcr'
 
 VCR.configure do |config|
