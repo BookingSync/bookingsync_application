bcb03c182cff5abcae18c491597c9943a93f2644
Mandaryn <mandaryyyn@gmail.com>
2015-05-21 11:55:15 +0200

    remove testing dependencies, they should not be loaded to production group

--- lib/bookingsync_application/spec_helper.rb
+++ lib/bookingsync_application/spec_helper.rb
@@ -5,17 +5,3 @@ shared_examples_for :synced_model do
     it { is_expected.to include Synced::HasSyncedData }
   end
 end
-
-require 'vcr'
-
-VCR.configure do |config|
-  config.cassette_library_dir = 'spec/fixtures/cassettes'
-  config.hook_into :webmock
-  config.configure_rspec_metadata!
-  config.filter_sensitive_data('Bearer <OAUTH_TOKEN>') do |interaction|
-    if interaction.request.headers['Authorization']
-      interaction.request.headers['Authorization'].first
-    end
-  end
-  config.ignore_hosts 'codeclimate.com'
-end
