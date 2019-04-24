56ba9269e9d11fcb3e6629d562b5e13229112a9e
Chambeur <marc.chamberlin@gmail.com>
2019-04-24 18:03:10 +0800

    fix sqlite load error

--- bookingsync_application.gemspec
+++ bookingsync_application.gemspec
@@ -27,7 +27,7 @@ Gem::Specification.new do |s|
   s.add_development_dependency 'appraisal'
   s.add_development_dependency 'rspec-rails'
   s.add_development_dependency 'factory_bot_rails'
-  s.add_development_dependency 'sqlite3'
+  s.add_development_dependency 'sqlite3', '~> 1.3.6'
   s.add_development_dependency 'vcr'
   s.add_development_dependency 'webmock'
   s.add_development_dependency 'rubocop'
--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -18,5 +18,9 @@ module Dummy
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    if Rails.version >= "5.1.0" && config.active_record.sqlite3.present?
+      config.active_record.sqlite3.represent_boolean_as_integer = true
+    end
   end
 end
