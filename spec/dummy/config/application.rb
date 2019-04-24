e47aa9644102256d3ab75f7d77d0670f315a7860
Chambeur <marc.chamberlin@gmail.com>
2019-04-24 17:27:27 +0800

    fix sqlite deprecation warning

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -19,6 +19,8 @@ module Dummy
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    Rails.application.config.active_record.sqlite3.represent_boolean_as_integer = true
+    if Rails.version >= "5.1.0"
+      config.active_record.sqlite3.represent_boolean_as_integer = true
+    end
   end
 end
